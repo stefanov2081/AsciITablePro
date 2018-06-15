2152c164b4b8356785701edf071f75e20c130177
mandjah <stefanov2081@gmail.com>
2018-06-15 16:25:21 +0300

    Ascii Character Details is ready

--- ascii-table-html/scripts/script.js
+++ ascii-table-html/scripts/script.js
@@ -1,4 +1,133 @@
-﻿function printAsciiTable(asciiTableArray, numberOfRows) {
+﻿// Copy to clipboard
+function clipboardCopy() {
+    "use strict"
+
+    var $asciiCardElement = $('.ascii-char-code-card');
+    var asciiCharValueElement = '.ascii-char-value';
+
+    $asciiCardElement.on('click', function () {
+
+        var asciiCharValueElementText = $(this).children(asciiCharValueElement).text();
+        var hiddenInputElement = document.createElement("input");
+
+        hiddenInputElement.setAttribute('value', asciiCharValueElementText);
+
+        document.body.appendChild(hiddenInputElement);
+
+        hiddenInputElement.select();
+
+        document.execCommand('copy');
+
+        hiddenInputElement.remove();
+    });
+}
+
+function showCopiedPopover() {
+    "use strict"
+
+    let $popoverElement = $('.ascii-char-code-card');
+    let $popoverInit = $('[data-toggle="popover"]');
+
+    $popoverInit.popover();
+
+    $popoverElement.on('click', function () {
+
+        let that = $(this);
+
+        that.popover();
+
+        setTimeout(function () {
+            that.popover('hide');
+        }, 1500);
+    });
+}
+
+// Ascii details 
+function createLiForAsciiDetails(label, value) {
+    "use strict"
+
+    var $li = $('<li>').addClass('list-inline-item');
+    var $divContainer = $('<div>').addClass('card ascii-char-code-card');
+    $divContainer.attr('data-toggle', 'popover');
+    $divContainer.attr('data-placement', 'bottom');
+    $divContainer.attr('data-content', 'Copied!');
+
+    var $divLabel = $('<div>').addClass('ascii-char-label');
+    var uppercaseRegex = /(?=[A-Z])/;
+    var labelSplit = label.split(uppercaseRegex).join(" ");
+    $divLabel.text(`${labelSplit}:`);
+
+    var $divValue = $('<div>').addClass('ascii-char-value').text(value);
+    var $divIcon = $('<div>').addClass('ascii-char-icon');
+    var $i = $('<i>').addClass('far fa-clipboard');
+
+    $divIcon.append($i);
+    $divContainer.append($divLabel);
+    $divContainer.append($divValue);
+    $divContainer.append($divIcon);
+    $li.append($divContainer);
+
+    return $li;
+}
+
+function printAsciiDetails(char) {
+    "use strict"
+
+    // Details header
+    var $asciiDetailsContainer = $('#ascii-details-container');
+    var $h5 = $('<h5>');
+    var span1Text;
+
+    if (char.abbreviation) {
+        span1Text = char.abbreviation + ' ';
+    } else {
+        span1Text = char.glyph + ' ';
+    }
+
+    $h5.append($('<span>').text(span1Text));
+
+    if (char.description) {
+        var $span2 = $h5.append($('<span>').text(char.description));
+    }
+
+    $asciiDetailsContainer.append($h5);
+
+    // Details body
+    var $ul = $('<ul>').addClass('list-inline');
+
+    for (var propertyKey in char) {
+
+        if (propertyKey !== 'charType' &&
+            propertyKey !== 'description' &&
+            propertyKey !== 'abbreviation' &&
+            typeof char[propertyKey] !== 'function' &&
+            char[propertyKey]) {
+
+            var $li = createLiForAsciiDetails(propertyKey, char[propertyKey]);
+
+            $ul.append($li);
+        }
+
+    }
+
+    $asciiDetailsContainer.append($ul);
+
+    clipboardCopy();
+    showCopiedPopover();
+}
+
+function deleteAllChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+function clearAsciiDetails() {
+    deleteAllChildren($('#ascii-details-container')[0]);
+}
+
+// Ascii table
+function printAsciiTable(asciiTableArray, numberOfRows) {
     "use strict"
 
     var $div = $('<div>').addClass('ascii-table-container row');
@@ -21,7 +150,8 @@
         $table.append($thead);
 
         for (var j = numberOfRows * i; j < numberOfRows * i + numberOfRows; j++) {
-            $tr = $('<tr>').addClass(asciiTableArray[j].charType);
+            $tr = $('<tr>').addClass(asciiTableArray[j].charType).attr('data-index', j);
+
             $tr.append($('<td>').append(asciiTableArray[j].dec));
             $tr.append($('<td>').append(asciiTableArray[j].glyph));
             $tbody.append($tr);
@@ -33,28 +163,6 @@
     }
 }
 
-function printAsciiDetails(char) {
-    "use strict"
-
-    // Details header
-    var $asciiDetailsContainer = $('#ascii-details-container');
-    var $h5 = $('<h5>');
-    var span1Text;
-    
-    if (char.abbreviation) {
-       span1Text = char.abbreviation + ' ';
-    } else {
-        span1Text = char.glyph + ' ';
-    }
-
-    $h5.append($('<span>').text(span1Text));
-
-    if (char.description) {
-        var $span2 = $h5.append($('<span>').text(char.description));
-    }
-
-    $asciiDetailsContainer.append($h5);
-}
 
 $(function () {
     "use strict"
@@ -62,4 +170,11 @@ $(function () {
     var asciiTableRepository = new AsciiTableRepository();
     printAsciiTable(asciiTableRepository.asciiTable, 32);
     printAsciiDetails(asciiTableRepository.asciiTable[1]);
+
+    var $tr = $('tr').click(function () {
+        console.log(this);
+
+        clearAsciiDetails();
+        printAsciiDetails(asciiTableRepository.asciiTable[$(this).attr('data-index')]);
+    });
 });
